7c7f7f721ce5598bfcd1b95c1dca9dc250a3c670
kenwheeler <dubmediagroup@gmail.com>
2014-03-09 00:53:21 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+    gulp.src([
+      'src/_reset.scss',
+      'src/_settings.scss',
+      'src/_functions.scss',
+      'src/_mixins.scss',
+      'src/_typography.scss'
+      ])
+      .pipe($.concat("guff.scss"))
+      .pipe(gulp.dest('.'))
+      .pipe($.size());
+});
+
+gulp.task('demo', function () {
+    gulp.src('demo/styles/main.scss')
+      .pipe($.sass({
+        style: 'compressed'
+      }))
+      .pipe(gulp.dest('demo/styles'))
+      .pipe($.size());
+});
+
+gulp.task('docs', function(){
+    gulp.src("./guff.scss")
+    .pipe($.docco())
+    .pipe(gulp.dest('./docs'))
+});
+
+gulp.task('default', ['styles','demo','docs']);
+
+gulp.task('connect', $.connect.server({
+    root: ['demo'],
+    port: 9000,
+    livereload: true
+}));
+
+gulp.task('watch', ['connect'], function () {
+    gulp.watch([
+        'demo/*.html',
+        'demo/styles/**/*.css',
+        'src/**/*.scss'
+    ], function(event) {
+        return gulp.src(event.path)
+            .pipe($.connect.reload());
+    });
+    gulp.watch(['src/**/*.scss'], ['default']);
+    gulp.watch(['demo/styles/**/*.scss'], ['default']);
+});
